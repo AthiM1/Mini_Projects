a40a346d08f8f1a7c71b7faec24d04d981e3eff0
AthiM1 <mbondwana.a.s@gmail.com>
2020-10-21 16:46:40 +0200

    first commit

--- Price_of_chair/src/app.py
+++ Price_of_chair/src/app.py
@@ -1,16 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-request = requests.get("http://www.johnlewis.com/john-lewis-wade-office-chair-black/p447855")
+request = requests.get("https://www.johnlewis.com/john-lewis-partners-brooks-side-dining-chairs-set-of-2/antique-brown/p3189169")
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
-element = soup.find("span", {"itemprop": "price", "class": "now-price"})
+element = soup.find("p", {"class": "u-centred price"})
 string_price = element.text.strip()
 
 price_without_symbol = string_price[1:]
 
 price = float(price_without_symbol)
-
+print(price)
 if price < 200:
     print("You should buy the chair")
     print("The current price is {}.".format(string_price))
